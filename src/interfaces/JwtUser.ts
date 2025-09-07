627e2e5111b3d4c5d7166bbf855c7bd723ed5a34
Naidelyn Maldonado <naidelyn.maldonado@cit.hn>
2025-09-06 23:58:07 -0600

    feat: agregar módulos y entidades para Combo, Dish, Product y RawMaterial, además de interfaces JwtUser y Metadata

---
+++ src/interfaces/JwtUser.ts
@@ -0,0 +1,7 @@
+export class JwtUser {
+    authUserId: number;
+    authOrgId: number;
+    authUserName: string;
+    authUserEmail: string;
+    authUserRole: string;
+}
---
+++ src/interfaces/Metadata.ts
@@ -0,0 +1,6 @@
+export class Metadata {
+    page: number;
+    pageSize: number;
+    totalCount: number;
+    totalPages: number;
+}
